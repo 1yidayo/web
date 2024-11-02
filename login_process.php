965b537f6a19d59879a816315ca5f080b7bcd590
ziqin030 <ziqin030@gmail.com>
2024-11-02 23:35:34 +0800

    參考用

--- login_process.php
+++ login_process.php
@@ -8,18 +8,12 @@ $password = $_POST['password'];
 // 檢查帳號密碼是否正確
 if ($account == "root" && $password == "password") {
     // 將帳號存入 session 變數
-    $_SESSION["account"] = $account;
-
-    // 檢查是否選擇「記住我」
-    if (isset($_POST['remember_me'])) {
-        // 設置 cookie，30 天有效
-        setcookie('remember_me', $account, time() + (86400 * 30), "/");
-    }
-
-    // 導向成功頁面
+    $_SESSION['is_login'] = true;
     header("Location: 預借教室.php?page=one");
+    // 導向成功頁面
     exit();
 } else {
+    $_SESSION['is_login'] = false;
     // 如果不匹配，跳轉回 login.php 並傳遞 msg 變數
     header("Location: login.php?msg=帳號或密碼錯誤");
     exit();
--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
-session_destroy(); // 銷毀會話
+session_unset();
 header("Location: login.php"); // 重定向到登入頁面
 exit();
